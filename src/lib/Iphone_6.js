77b7a86643b461c2c3373fd5e467e0b2f93e1331
Vinicius Rocha <viniciusrocha0996@gmail.com>
2018-11-27 14:39:51 -0200

    finished migration of iphone

--- src/lib/Iphone_6.js
+++ src/lib/Iphone_6.js
@@ -29,15 +29,98 @@ const Screen = styled.div`
   border: 1px solid #000;
 `;
 
+const Speaker = styled.div`
+  width: 20%;
+  background: #666;
+  height: 14%;
+  border-radius: 3px;
+  margin: 0.8vh auto;
+
+  ::after {
+    width: 0.5vh;
+    background: #666;
+    height: 0.5vh;
+    border-radius: 50%;
+    margin: 3% auto;
+    top: 3%;
+    position: absolute;
+    content: "";
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  ::before {
+    width: 0.7vh;
+    background: #666;
+    height: 0.7vh;
+    border-radius: 50%;
+    margin: 0.7vh;
+    top: 5.8%;
+    position: absolute;
+    content: "";
+    left: 33%;
+    transform: translateX(-50%);
+  }
+`;
+
+const Button = styled.div`
+  align-self: center;
+  margin: auto;
+  height: 3.3vh;
+  width: 3.3vh;
+  border-radius: 50%;
+  background: linear-gradient(#dfdfdf, #fefefe);
+  border: 1px solid #ccc;
+`;
+
+const Power = styled.div`
+  background: #bbb;
+  position: absolute;
+  width: 0.4vh;
+  height: 8%;
+  right: -0.3vh;
+  top: 23%;
+`;
+
+const Volume = styled.div`
+  background: #bbb;
+  position: absolute;
+  width: 0.4vh;
+  height: 6%;
+  left: -0.3vh;
+  top: 23%;
+  content: "";
+
+  ::before {
+    background: #bbb;
+    position: absolute;
+    width: 0.4vh;
+    height: 70%;
+    top: -150%;
+    content: "";
+  }
+
+  ::after {
+    background: #bbb;
+    position: absolute;
+    width: 0.4vh;
+    height: 100%;
+    top: 150%;
+    content: "";
+  }
+`;
+
 class Iphone_6 extends Component {
   render() {
     return (
       <Container>
         <Iphone>
           <div />
-          <div />
+          <Speaker />
           <Screen />
-          <div />
+          <Button />
+          <Power />
+          <Volume />
         </Iphone>
       </Container>
     );
