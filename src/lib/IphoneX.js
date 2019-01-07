0037235aa0314f724a046cb67f4ce835741514b9
Vinicius Rocha <viniciusrocha0996@gmail.com>
2019-01-07 16:26:45 -0200

    added the notch

--- src/lib/IphoneX.js
+++ src/lib/IphoneX.js
@@ -9,12 +9,14 @@ const Case = styled.div`
   padding: 28px;
   border-radius: 60px;
   box-shadow: inset 0px 0px 0px 2px #000, inset 0px 0px 0px 6px #e4e4e4,
-    inset 0px 0px 0px 7px #000, inset 5px 0px 1px 5px #2f2f2f,
-    inset -5px 0px 1px 5px #2f2f2f, inset 0px 0px 3px 10px #5c5c5c,
-    inset 0px 0px 0px 11px #000, inset 10px 0px 3px 6px #afafaf,
+    inset 0px 0px 0px 7px #000, inset 5px 0px 1px 6px #2f2f2f,
+    inset -5px 0px 1px 5px #2f2f2e, inset 1px 0px 0px 10px #5c5c5c,
+    inset 0px 0px 0px 12px #000, inset 10px 0px 3px 6px #afafaf,
     inset 10px 3px 8px #7b7b7b, inset 10px 0px 3px 9px #585858,
-    inset -10px 0px 3px 6px #afafaf, inset -10px 3px 8px #7b7b7b,
-    inset -10px 0px 3px 9px #585858;
+    inset -10px 0px 3px 6px #afafaf, inset -10px 3px 8px #7b7b78,
+    inset -10px 0px 3px 9px #585858, inset -20px 0px 13px 11px #0f0f0f,
+    inset 20px 0px 7px 11px #0f0f0f, inset 0px 0px 0px 19px #636363;
+  display: relative;
 `;
 
 const Screen = styled.div`
@@ -22,13 +24,38 @@ const Screen = styled.div`
   height: 100%;
   width: 100%;
   border-radius: 35px;
+  position: relative;
+`;
+
+const Notch = styled.div`
+  position: absolute;
+  width: 210px;
+  height: 30px;
+  background: #000;
+  content: "";
+  right: 50%;
+  transform: translateX(50%);
+  border-radius: 0 0 25px 25px;
+
+  ::before {
+    content: "";
+    position: absolute;
+    width: 42px;
+    height: 9px;
+    background: #222;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
 
 class IphoneX extends Component {
   render() {
     return (
       <Case>
-        <Screen />
+        <Screen>
+          <Notch />
+        </Screen>
       </Case>
     );
   }
